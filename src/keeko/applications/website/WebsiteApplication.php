a50c33f1d471ba4669c6377c3d479e6c358722cb
gossi <github@gos.si>
2013-08-08 20:38:54 +0200

    First Hello World

--- src/keeko/applications/website/WebsiteApplication.php
+++ src/keeko/applications/website/WebsiteApplication.php
@@ -1,7 +1,26 @@
 <?php
 namespace keeko\applications\website;
 
+use keeko\core\application\AbstractApplication;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use keeko\core\routing\ApplicationRouter;
 
-class WebsiteApplication {
+class WebsiteApplication extends AbstractApplication {
+
+	
+	/* (non-PHPdoc)
+	 * @see \keeko\core\application\AbstractApplication::doRun()
+	 */
+	protected function doRun(Request $request, Response $response, ApplicationRouter $appRouter) {
+		$response->setContent('Hello World');
+		return $response;
+		
+// 		$handler = $this->router->getHandler();
+// 		$match = $this->router->match($appRouter->getDestination());
+		
+// 		$main = $handler->getMainContent($match);
+	}
+
 
 }
