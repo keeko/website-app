e4821cbf756817b00747c8f2cce3f9e20b3c2ad5
gossi <github@gos.si>
2013-08-18 02:51:32 +0200

    First Hello World with Design

--- src/keeko/applications/website/WebsiteApplication.php
+++ src/keeko/applications/website/WebsiteApplication.php
@@ -5,6 +5,8 @@ use keeko\core\application\AbstractApplication;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use keeko\core\routing\ApplicationRouter;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 
 class WebsiteApplication extends AbstractApplication {
 
@@ -13,13 +15,43 @@ class WebsiteApplication extends AbstractApplication {
 	 * @see \keeko\core\application\AbstractApplication::doRun()
 	 */
 	protected function doRun(Request $request, Response $response, ApplicationRouter $appRouter) {
-		$response->setContent('Hello World');
+// 		$response->setContent('Hello World');
+// 		return $response;
+		
+		
+		try {
+			$handler = $this->router->getHandler();
+			$match = $this->router->match($appRouter->getDestination());
+
+			
+			// get design
+			$design = $this->application->getDesign();
+			$designPath = KEEKO_PATH_DESIGNS . '/' . $design->getName() . '/';
+
+			
+			// get layout from handler
+			$layout = $handler->getLayout($match);
+			$layout = $layout === null ? 'main' : $layout;
+			
+			$layoutDir = $designPath . '/templates';
+			
+			
+			// get contents
+			$contents = $handler->getContents($match);
+			
+			
+			$loader = new \Twig_Loader_Filesystem($layoutDir);
+			$twig = new \Twig_Environment($loader);
+			
+			$response->setContent($twig->render($layout . '.twig', $contents));
+		
+		} catch (ResourceNotFoundException $e) {
+			$response->setStatusCode(404);
+		} catch (MethodNotAllowedException $e) {
+			$response->setStatusCode(405);
+		}
+		
 		return $response;
-		
-// 		$handler = $this->router->getHandler();
-// 		$match = $this->router->match($appRouter->getDestination());
-		
-// 		$main = $handler->getMainContent($match);
 	}
 
 
